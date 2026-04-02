0de44102eb639515087dcc4fe0e24b2d0cc0a237
siligurisasta <siligurisasta@gmail.com>
2026-04-02 15:03:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -542,14 +542,20 @@ function loadOrders(){
       snapshot.forEach(doc=>{
         const o = doc.data();
 
-        html += `
-          <div>
-            <b>ID:</b> ${o.orderId}<br>
-            <b>Name:</b> ${o.name}<br>
-            <b>Total:</b> ₹${o.total}<br>
-            <b>Status:</b> ${o.status}
-          </div>
-        `;
+        let itemsText = "";
+o.items.forEach(i=>{
+  itemsText += `${i.name} × ${i.qty}<br>`;
+});
+
+html += `
+  <div style="border:1px solid #ddd;padding:10px;margin:5px;border-radius:10px">
+    <b>ID:</b> ${o.orderId}<br>
+    <b>Name:</b> ${o.name}<br>
+    <b>Items:</b><br>${itemsText}
+    <b>Total:</b> ₹${o.total}<br>
+    <b>Status:</b> ${o.status}
+  </div>
+`;
       });
 
       document.getElementById("ordersList").innerHTML = html;
